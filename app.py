3caf95520590b0dab59b48c03aa5aabe3cd097aa
Tilakakasapu <tilak57233@gmail.com>
2024-12-01 17:18:23 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import numpy as np
+import pickle
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+model = load_model('next_word_lstm.h5')
+
+
+with open('tokenizer.pickle','rb') as handle:
+    tokenizer = pickle.load(handle)
+
+def predict_next_word(model, tokenizer, text, max_sequence_len):
+    token_list = tokenizer.texts_to_sequences([text])[0]
+    if len(token_list) >= max_sequence_len:
+        token_list = token_list[-(max_sequence_len-1):]  # Ensure the sequence length matches max_sequence_len-1
+    token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+    predicted = model.predict(token_list, verbose=0)
+    predicted_word_index = np.argmax(predicted, axis=1)
+    for word, index in tokenizer.word_index.items():
+        if index == predicted_word_index:
+            return word
+    return None
+
+
+
+
+st.set_page_config(page_title="Next Word Prediction", layout="centered")
+st.title("Next Word Prediction")
+st.subheader("Enter a sentence, and I will predict the next word!")
+
+user_input = st.text_area("Enter your sentence:", "", height=100)
+
+if st.button("Predict Next Word"):
+    if user_input:
+        
+        next_word = predict_next_word(model,tokenizer,user_input,model.input_shape[1]+1)
+        st.markdown(f"**Next word prediction:** {next_word}")
+    else:
+        st.error("Please enter a sentence to predict the next word.")
+
+# Optionally, add more features such as displaying model details, etc.
+st.sidebar.header("About")
+st.sidebar.text("This is a simple Streamlit app for Next Word Prediction using a trained language model.")
